5f4e3e1b5cfb4f597547be4cb8b37887beb09087
RobertZ2011 <robert@zieba.site>
2018-04-18 21:20:00 -0500

    Last commit before merge

--- src/SingleGate.hpp
+++ src/SingleGate.hpp
@@ -2,36 +2,6 @@
 #define QATUX_SINGLE_GATE
 
 namespace Qatux {
-    /*template<int Q, int NQUBITS, typename T>
-    struct SingleGate {
-        static inline Vector<pow2(NQUBITS), T> calculate(const Vector<pow2(NQUBITS), T>& state, const Eigen::Matrix<Complex<T>, 2, 2>& op) {
-            constexpr int aboveCount = NQUBITS - (Q + 1);
-            constexpr int belowCount = Q;
-            return kroneckerProduct(IdentityGate<aboveCount, T>::value(), kroneckerProduct(op, IdentityGate<belowCount, T>::value())) * state;
-        }
-    };
-
-    template<typename T>
-    struct SingleGate<0, 1, T> {
-        static inline Vector<2, T> calculate(const Vector<2, T>& state, const Eigen::Matrix<Complex<T>, 2, 2>& op) {
-            return op * state;
-        }
-    };
-
-    template<int NQUBITS, typename T>
-    struct SingleGate<0, NQUBITS, T> {
-        static inline Vector<pow2(NQUBITS), T> calculate(const Vector<pow2(NQUBITS), T>& state, const Eigen::Matrix<Complex<T>, 2, 2>& op) {
-            return kroneckerProduct(IdentityGate<NQUBITS - 1, T>::value(), op) * state;
-        }
-    };
-
-    template<int NQUBITS, typename T>
-    struct SingleGate<NQUBITS - 1, NQUBITS, T> {
-        static inline Vector<pow2(NQUBITS), T> calculate(const Vector<pow2(NQUBITS), T>& state, const Eigen::Matrix<Complex<T>, 2, 2>& op) {
-            return kroneckerProduct(op, IdentityGate<NQUBITS - 1, T>::value()) * state;
-        }
-    };*/
-
     template<typename T>
     Vector<T> singleGate(int Q, int NQUBITS, const Vector<T>& state, const Matrix<T>& op) {
         if(Q == 0 && NQUBITS == 1) {
